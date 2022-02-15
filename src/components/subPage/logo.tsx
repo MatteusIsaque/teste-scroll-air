4836f7f435a14f04d3d588a1ad29eae4c5c63136
matteus isaque <matteus.isaque28@gmail.com>
2022-02-15 09:18:05 -0300

    ok

--- src/components/subPage/logo.tsx
+++ src/components/subPage/logo.tsx
@@ -1,13 +1,13 @@
 import styles from './../../styles/subPages/logo.module.scss'
 import Image from "next/image";
 
-import LogoPng from './../../img/ticonnected-logo.png'
+// import LogoPng from './../../img/ticonnected-logo.png'
 
 export default function Logo() {
 
   return (
     <div className={styles.main}>
-      <Image src={LogoPng} width={120} height={42} />
+      {/* <Image src={LogoPng} width={120} height={42} /> */}
     </div>
   )
 }
